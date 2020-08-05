82f8f7b53ccb878dbfb554e816eeaa58c3b31d34
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-08-05 16:53:28 -0700

    Histogram done

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,19 @@
-# Your code here
+import re
 
+with open("robin.txt") as f:
+    words = f.read()
+
+def word_count(s):
+    cache = {}
+    my_string = re.sub(r'[^A-Za-z0-9\' ]+', ' ', s).lower().split()
+
+    for word in my_string:
+        if word not in cache:
+            cache[word] = '#'
+        else:
+            cache[word] += '#'
+    return sorted(cache.items(), key=lambda x: x[1], reverse=True)
+
+
+for word,rank in word_count(words):
+  print(word, rank)
